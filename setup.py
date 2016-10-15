03a9ff992b6bbfb013a780ea2604cd6391700161
Kevin Coyner <kcoyner@debian.org>
2016-10-15 09:31:49 -0400

    Add to initial move to github.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+DESC = """Free replacement for rpl (replace strings in files)."""
+
+setup(name='rpl',
+	  version='1.5.6',
+	  description=DESC,
+	  author='Goran Weinholt',
+      url='https://github.com/kcoyner/rpl',
+	  license='GPL v2 or later',
+	  scripts=['rpl'])
