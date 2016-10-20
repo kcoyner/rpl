51bce6352eaee81701e3bd2219a03416cf5d7d00
Kevin Coyner <kcoyner@debian.org>
2016-10-19 23:31:56 -0400

    Modified rpl to run using python3 as well as python2.7 or greater. Changed setup.py to use setuptools.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rpl
-intelligent recursive search/replace utility
+An intelligent recursive search/replace utility written in Python
+
+rpl is a text replacement utility. It will replace strings with new strings in
+multiple text files. It can work recursively over directories and supports
+limiting the search to specific file suffixes.
+
--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
+
+try:
+    def u8(s):
+        return s.decode('unicode-escape')
+except NameError:
+    def u8(s):
+        return s
 
 DESC = """Free replacement for rpl (replace strings in files)."""
 
 setup(name='rpl',
-	  version='1.5.6',
-	  description=DESC,
-	  author='Goran Weinholt',
+      version='1.5.6',
+      description=DESC,
+      author=u8('Göran Weinholt'),
+      author_email=u8('weinholt@debian.org'),
       url='https://github.com/kcoyner/rpl',
-	  license='GPL v2 or later',
-	  scripts=['rpl'])
+      license='GPL v2 or later',
+      scripts=['rpl'],
+      classifiers=[
+          'Environment :: Console',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+     )
