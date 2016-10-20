5ed9117a35e966c3b58a78795684bc5a9d099d8c
Kevin Coyner <kcoyner@debian.org>
2016-10-20 10:08:45 -0400

    Add copyright/license info and long description to setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+# Copyright (c) 2016 Kevin Coyner <kcoyner@debian.org>
+#
+# This file is part of rpl.
+# Redistributable under the GNU General Public License
+# https://www.gnu.org/licenses/#GPL
+
 from setuptools import setup
 
 DESC = """Free replacement for rpl (replace strings in files)."""
 
+long_description =  (open('README.md').read()
+
 setup(name='rpl',
       version='1.5.6',
       description=DESC,
+	  long_description=long_description,
       author=u'Göran Weinholt',
       author_email=u'weinholt@debian.org',
       url='https://github.com/kcoyner/rpl',
