dcb89913ee2d1fdf3c0021511b1b683705c705ef
Kevin Coyner <kcoyner@debian.org>
2016-10-20 07:01:28 -0400

    Removed unicode function.

--- setup.py
+++ setup.py
@@ -3,20 +3,13 @@
 
 from setuptools import setup
 
-try:
-    def u8(s):
-        return s.decode('unicode-escape')
-except NameError:
-    def u8(s):
-        return s
-
 DESC = """Free replacement for rpl (replace strings in files)."""
 
 setup(name='rpl',
       version='1.5.6',
       description=DESC,
-      author=u8('Göran Weinholt'),
-      author_email=u8('weinholt@debian.org'),
+      author=u'Göran Weinholt',
+      author_email=u'weinholt@debian.org',
       url='https://github.com/kcoyner/rpl',
       license='GPL v2 or later',
       scripts=['rpl'],
