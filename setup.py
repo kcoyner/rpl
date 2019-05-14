f202822fef721e5723d754feb2b9c4e70eb2136c
Reuben Thomas <rrt@sc3d.org>
2019-05-14 11:07:55 +0100

    Bump version to 1.5.8, and fix coding: tag in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 # Copyright (c) 2016 Kevin Coyner <kcoyner@debian.org>
 #
@@ -14,7 +14,7 @@ DESC = """Free replacement for rpl (replace strings in files)."""
 long_description =  open('README.md').read()
 
 setup(name='rpl',
-      version='1.5.6',
+      version='1.5.8',
       description=DESC,
       long_description=long_description,
       author=u'Göran Weinholt',
