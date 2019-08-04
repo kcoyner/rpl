20468ae9488cabb019d4ecd72e0fec64495c5faa
Reuben Thomas <rrt@sc3d.org>
2019-08-04 22:19:29 +0200

    Add automatic text encoding detection with chardet
    And a manual --encoding flag

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ DESC = """Free replacement for rpl (replace strings in files)."""
 long_description =  open('README.md').read()
 
 setup(name='rpl',
-      version='1.5.8',
+      version='1.6.0',
       description=DESC,
       long_description=long_description,
       author=u'Göran Weinholt',
@@ -22,6 +22,7 @@ setup(name='rpl',
       url='https://github.com/kcoyner/rpl',
       license='GPL v3 or later',
       scripts=['rpl'],
+      install_requires=['chardet']
       classifiers=[
           'Environment :: Console',
           'Programming Language :: Python :: 3',
