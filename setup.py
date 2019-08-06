a20902365e25cf41336d8052e456f888fbf0928e
Reuben Thomas <rrt@sc3d.org>
2019-08-06 10:33:13 +0200

    setup.py: remove license (it doesn’t need one)
    Also inline the description (was defined as a variable and then used once).

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-# Copyright (c) 2016 Kevin Coyner <kcoyner@debian.org>
-#
-# This file is part of rpl.
-# Redistributable under the GNU General Public License
-# https://www.gnu.org/licenses/#GPL
 
 from setuptools import setup
-
-DESC = """Replace strings in files."""
 
 long_description = open('README.md').read()
 
 setup(name='rpl',
       version='1.6.0',
-      description=DESC,
+      description="""Replace strings in files.""",
       long_description=long_description,
       maintainer='Reuben Thomas',
       maintainer_email='rrt@sc3d.org',
