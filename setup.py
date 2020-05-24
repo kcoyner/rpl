cdda539c5dbd43bfa3300b284d91929fbd25e50f
Reuben Thomas <rrt@sc3d.org>
2020-05-24 23:05:03 +0100

    Bump version to 1.6.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 long_description = open('README.md').read()
 
 setup(name='rpl',
-      version='1.6.5',
+      version='1.6.6',
       description="""Replace strings in files.""",
       long_description=long_description,
       maintainer='Reuben Thomas',
