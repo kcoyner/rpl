5f8562e6e9379681fa9728dc53f97e84062c41c3
Reuben Thomas <rrt@sc3d.org>
2020-09-01 17:15:08 +0100

    Bump version to 1.7.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 long_description = open('README.md').read()
 
 setup(name='rpl',
-      version='1.7.1',
+      version='1.7.2',
       description="""Replace strings in files.""",
       long_description=long_description,
       maintainer='Reuben Thomas',
