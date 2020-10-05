cfda32bdfe0939b00c5d8b531e004feab303e257
Reuben Thomas <rrt@sc3d.org>
2020-10-05 12:31:37 +0100

    Move setup to setup.cfg; install man page with argparse-manpage
    distutils works, but cannot be used conveniently to publish to pypi, so find
    a more modern way.

--- setup.py
+++ setup.py
@@ -1,22 +1,9 @@
-#!/usr/bin/env python3
+from build_manpages.build_manpages import get_install_cmd
+from setuptools import setup
+from setuptools.command.install import install
 
-from distutils.core import setup
-
-long_description = open('README.md').read()
-
-setup(name='rpl',
-      version='1.7.2',
-      description="""Replace strings in files.""",
-      long_description=long_description,
-      maintainer='Reuben Thomas',
-      maintainer_email='rrt@sc3d.org',
-      url='https://github.com/rrthomas/rpl',
-      license='GPL v3 or later',
-      scripts=['rpl'],
-      data_files=[('share/man/man1', ['rpl.1'])],
-      install_requires=['chardet'],
-      classifiers=[
-          'Environment :: Console',
-          'Programming Language :: Python :: 3',
-      ],
-     )
+setup(
+    cmdclass={
+        'install': get_install_cmd(install),
+    }
+)
